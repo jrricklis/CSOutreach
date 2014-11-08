b8613af7583dd525e66e0d07530ce21ab9273217
jrricklis <jrricklis@gmail.com>
2014-11-07 19:02:56 -0600

    Additional footer tweaks.

--- CSOutreach/Site.Master.designer.cs
+++ CSOutreach/Site.Master.designer.cs
@@ -22,6 +22,15 @@ namespace CSOutreach {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder HeadContent;
         
         /// <summary>
+        /// pageform control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm pageform;
+        
+        /// <summary>
         /// login control.
         /// </summary>
         /// <remarks>
